f6f779b128ef9db3a44df16b663a11c827b27258
Saksham Joshi <150180744+sakshamjosh1@users.noreply.github.com>
2024-06-30 22:28:45 +0530

    Add files via upload

---
+++ JavaScript/Basics/JS Code/class.js
@@ -0,0 +1,21 @@
+// Class
+class Person {
+    constructor(firstName, lastName, dob) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = new Date(dob);
+    }
+    getBirthYear() {
+        return this.dob.getFullYear();
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+//Instantiate Objects
+const person1 = new Person('John', 'Doe', '4-3-1980');
+const person2 = new Person('Mary', 'Smith', '3-6-1970');
+
+console.log(person2.getFullName());
+console.log(person1);
